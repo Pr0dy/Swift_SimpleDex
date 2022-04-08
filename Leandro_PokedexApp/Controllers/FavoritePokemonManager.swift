5a303a092f27f77a3f693fd981e7563d056fabf5
Leandro <leandro.miguel.banha@gmail.com>
2022-04-08 15:31:46 +0100

    added favoritePokemonManager class

---
+++ Leandro_PokedexApp/Controllers/FavoritePokemonManager.swift
@@ -0,0 +1,21 @@
+//
+//  FavoritePokemonManager.swift
+//  Leandro_PokedexApp
+//
+//  Created by leandro.banha on 08/04/2022.
+//
+
+import Foundation
+
+class FavoritePokemonManager{
+    
+    var favoritePokemonList: [Int:PokemonModel]?
+    
+    func addFavoritePokemonToList(pokemon: PokemonModel, index: Int){
+        
+    }
+    
+    func removeFavoritePokemonToList(index: Int){
+        
+    }
+}
